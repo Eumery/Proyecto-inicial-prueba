5c7c7f8794e4edaee4f63b771fc6325bad869ce8
EumeryW <eumeryward@gmail.com>
2023-08-27 18:09:50 -0300

    muchos cambios

--- js/categories.js
+++ js/categories.js
@@ -40,6 +40,7 @@ function setCatID(id) {
     window.location = "products.html"
 }
 
+
 function showCategoriesList(){
 
     let htmlContentToAppend = "";
@@ -49,27 +50,56 @@ function showCategoriesList(){
         if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
             ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
 
-            htmlContentToAppend += `
-            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${category.name}</h4>
-                            <small class="text-muted">${category.productCount} artículos</small>
-                        </div>
-                        <p class="mb-1">${category.description}</p>
-                    </div>
-                </div>
-            </div>
-            `
-        }
-
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
-    }
+                htmlContentToAppend += `
+                <div onclick="setCatID(${category.id})"  class="col" id="container" >
+                   <div class="card" style="width: 18rem;" style="border: 5px black;" style=":hover: ">
+                       <img src="${category.imgSrc}" alt="${category.description}" class="card-img-top">
+                       <div class="card-body">
+                               <h4>${category.name}</h4>
+                               <small class="card-text">${category.productCount} artículos</small>
+                           <p>${category.description}</p>
+                       </div>
+                   </div>
+               </div>
+               `
+           }
+   
+           document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+       }
+        
 }
+
+// function showCategoriesList(){
+
+//     let htmlContentToAppend = "";
+//     for(let i = 0; i < currentCategoriesArray.length; i++){
+//         let category = currentCategoriesArray[i];
+
+//         if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
+//             ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+
+//                 htmlContentToAppend += `
+//                 <div onclick="setCatID(${category.id})"  class="col" >
+                   
+//                    <div class="card" style="width: 18rem;" style="border: 5px black;">
+//                        <img src="${category.imgSrc}" alt="${category.description}" class="card-img-top">
+                       
+//                        <div class="card-body">
+                           
+//                                <h4>${category.name}</h4>
+//                                <small class="card-text">${category.productCount} artículos</small>
+                          
+//                            <p>${category.description}</p>
+//                        </div>
+//                    </div>
+//                </div>
+//                `
+//            }
+   
+//            document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+//        }
+        
+// }
 
 function sortAndShowCategories(sortCriteria, categoriesArray){
     currentSortCriteria = sortCriteria;
